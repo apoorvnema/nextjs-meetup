cf1a6b49ec3485bc199cfe7db7c200317a5f2185
apoorvnema <apoorvnema38@gmail.com>
2024-10-13 22:40:19 +0530

    [UPDATE]: Meet Details

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MeetupDetail from '../../components/meetups/MeetupDetail'
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail img="https://images.unsplash.com/photo-1517245386807-bb43f82c33c4" title="Tech Conference 2024" address="1234 Silicon Valley, CA, USA" description="1234 Silicon Valley, CA, USA"/>
+  )
+}
+
+export default MeetupDetails
