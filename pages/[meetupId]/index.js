8eef4741f59d77521477619705ffcf290d011a3d
apoorvnema <apoorvnema38@gmail.com>
2024-10-13 23:32:32 +0530

    [UPDATE]: SSR & SSG

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,10 +1,38 @@
 import React from 'react'
 import MeetupDetail from '../../components/meetups/MeetupDetail'
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
   return (
-    <MeetupDetail img="https://images.unsplash.com/photo-1517245386807-bb43f82c33c4" title="Tech Conference 2024" address="1234 Silicon Valley, CA, USA" description="1234 Silicon Valley, CA, USA"/>
+    <MeetupDetail img={props.meetupData.image} title={props.meetupData.title} address={props.meetupData.address} description={props.meetupData.description}/>
   )
+}
+
+export const getStaticPaths = async () => {
+    const sampleMeetups = ['m1','m2','m3'];
+
+    const paths = sampleMeetups.map((meetup) => ({
+        params: {meetupId: meetup}
+    }))
+    return {
+        fallback: false,
+        paths
+    }
+}
+
+export const getStaticProps = async (context) => {
+    const meetupId = context.params.meetupId
+
+    return {
+        props: {
+            meetupData: {
+                id: meetupId,
+                image: 'https://images.unsplash.com/photo-1517245386807-bb43f82c33c4',
+                title: 'Tech Conference 2024',
+                address: '1234 Silicon Valley, CA, USA',
+                description: '1234 Silicon Valley, CA, USA'
+            }
+        }
+    }
 }
 
 export default MeetupDetails
--- pages/index.js
+++ pages/index.js
@@ -2,31 +2,40 @@ import MeetupList from "../components/meetups/MeetupList";
 
 import React from 'react'
 
-const HomePage = () => {
-    const sampleMeetups = [
-        {
-          id: 'm1',
-          image: 'https://images.unsplash.com/photo-1517245386807-bb43f82c33c4',
-          title: 'Tech Conference 2024',
-          address: '1234 Silicon Valley, CA, USA',
-        },
-        {
-          id: 'm2',
-          image: 'https://images.unsplash.com/photo-1551836022-d5d88e9218df',
-          title: 'AI and Robotics Expo',
-          address: '5678 Innovation Park, NY, USA',
-        },
-        {
-          id: 'm3',
-          image: 'https://images.unsplash.com/photo-1551836022-d5d88e9218df',
-          title: 'Next.js Developer Meetup',
-          address: '91011 Dev Plaza, SF, USA',
-        },
-      ];      
+const HomePage = (props) => {
 
-    return (
-        <MeetupList meetups={sampleMeetups} />
-    )
+  return (
+    <MeetupList meetups={props.meetups} />
+  )
+}
+
+export async function getStaticProps() {
+  const sampleMeetups = [
+    {
+      id: 'm1',
+      image: 'https://images.unsplash.com/photo-1517245386807-bb43f82c33c4',
+      title: 'Tech Conference 2024',
+      address: '1234 Silicon Valley, CA, USA',
+    },
+    {
+      id: 'm2',
+      image: 'https://images.unsplash.com/photo-1551836022-d5d88e9218df',
+      title: 'AI and Robotics Expo',
+      address: '5678 Innovation Park, NY, USA',
+    },
+    {
+      id: 'm3',
+      image: 'https://images.unsplash.com/photo-1551836022-d5d88e9218df',
+      title: 'Next.js Developer Meetup',
+      address: '91011 Dev Plaza, SF, USA',
+    },
+  ];
+  return {
+    props: {
+      meetups: sampleMeetups
+    },
+    revalidate: 1
+  }
 }
 
 export default HomePage
